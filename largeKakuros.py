8fbcd052a1eb2453b68760677cd6126f3ff34902
georgestamatelis <52538570+georgestamatelis@users.noreply.github.com>
2019-12-17 12:19:18 +0200

    Add files via upload

--- largeKakuros.py
+++ largeKakuros.py
@@ -1,78 +1,88 @@
 from algorithms import *
 import time
-
-def sixX6KakuroConstraints(csp,setVar,setVal,var,val):
+def largeKakuroConstraints(csp,setVar,setVal,var,val):
+    """
+    """
     if(setVal==val):
         return False
     tester=csp.infer_assignment()
-    tester[var]=val
     tester[setVar]=setVal
-    if all(t in tester for t in("3,3","3,4","3,5","3,6")) in tester:
-        #print "fok"
-        sum=tester["3,3"]+tester["3,4"]+tester["3,5"]+tester["3,6"]
-        if(sum!=27):
+    tester[var]=val
+    if "2,2" in tester and "2,3" in tester:
+        if(tester["2,2"]+tester["2,3"]!=8):
             return False
-    if all(t in tester for t in("2,3","2,4","2,5","2,6")) in tester:
-        #print "fok"
-        sum=tester["2,3"]+tester["2,4"]+tester["2,5"]+tester["2,6"]
-        if(sum!=11):
+    if "2,5" in tester and "2,6" in tester:
+        if(tester["2,5"]+tester["2,6"]!=9):
             return False
-    if all(t in tester for t in("4,2","4,3")):
-        print "fok"
-        sum=tester["4,2"]+tester["4,3"]
-        if(sum!=17):
+    if "3,2" in tester and "3,3" in tester and "3,4" in tester and "3,5" in tester and "3,6" in tester:
+        if(tester["3,2"]+tester["3,3"]+tester["3,4"]+tester["3,5"]+tester["3,6"]!=30):
             return False
-    if all(t in tester for t in("5,2","5,3","5,4","5,5","5,6")):
-        #print "fok"
-        sum=tester["5,2"]+tester["5,3"]+tester["5,4"]+tester["5,5"]+tester["5,6"]
-        if(sum!=30):
+    if "4,3" in tester and "4,4" in tester and "4,5" in tester:
+        if(tester["4,3"]+tester["4,4"]+tester["4,5"]!=17):
             return False
-    if all(t in tester for t in("6,2","6,3","6,4","6,5","6,6")):
-        #print "fok"
-        sum=tester["6,2"]+tester["6,3"]+tester["6,4"]+tester["6,5"]+tester["6,6"]
-        if(sum!=10):
+    if "5,2" in tester and "5,3" in tester and "5,4" in tester and "5,5" in tester and "5,6" in tester:
+        if(tester["5,2"]+tester["5,3"]+tester["5,4"]+tester["5,5"]+tester["5,6"]!=35):
+            return False
+    if "6,2" in tester and "6,3" in tester:
+        if(tester["6,2"]+tester["6,3"]!=4):
+            return False
+    if "6,5" in tester and "6,6" in tester:
+        if(tester["6,5"]+tester["6,6"]!=9):
+            return False
+    ######VERTICAL SUMS#######################
+    if "2,2" in tester and "3,2" in tester:
+        if(tester["2,2"]+tester["3,2"]!=6):
+            return False
+    if "5,2" in tester and "6,2" in tester:
+        if(tester["5,2"]+tester["6,2"]!=9):
+            return False
+    if all(x in tester for x in ["2,3","3,3","4,3","5,3","6,3"]):
+        if(tester["2,3"]+tester["3,3"]+tester["4,3"]+tester["5,3"]+tester["6,3"]!=26):
+            return False
+    if all(x in tester for x in ["2,5","3,5","4,5","5,5","6,5"]):
+        if(tester["2,5"]+tester["3,5"]+tester["4,5"]+tester["5,5"]+tester["6,5"]!=31):
+            return False
+    if "3,4" in tester and "4,4" in tester and "5,4" in tester:
+        if(tester["3,4"]+tester["4,4"]+tester["5,4"]!=12):
+            return False
+    if "2,6" in tester and "3,6" in tester:
+        if(tester["2,6"]+tester["3,6"]!=12):
+            return False
+    if "6,6" in tester and "5,6" in tester:
+       if(tester["5,6"]+tester["6,6"]!=16):
             return False
     return True
-def sixX6KakuroSolver():
-    Variables=[
-    "3,3","3,4","3,5","3,6",
-    "2,3","2,4","2,5","2,6",
-    "4,2","4,3","4,5","4,6",
-    "5,2","5,3","5,4","5,5","5,6",
-    "6,2","6,3","6,4","6,5","6,6"
-    ]
-    #FIX THE NEIGHBOURS
-    neighbors={
-    "3,3":["3,4","3,5","3,6"],
-    "3,4":["3,3","3,5","3,6"],
-    "3,5":["3,3","3,4","3,6"],
-    "3,6":["3,3","3,4","3,5",],
-    ############################
-    "2,3":["2,4","2,5","2,6"],
-    "2,4":["2,3","2,5","2,6"],
-    "2,5":["2,4","2,3","2,6"],
-    "2,6":["2,4","2,5","2,3"],
-    ###########################
-    "4,2":["4,3","4,5","4,6"],
-    "4,3":["4,2","4,5","4,6"],
-    "4,5":["4,3","4,2","4,6"],
-    "4,6":["4,3","4,5","4,2"],
-    #############################
-    "5,2":["5,3","5,4","5,5","5,6"],
-    "5,3":["5,2","5,4","5,5","5,6"],
-    "5,4":["5,3","5,2","5,5","5,6"],
-    "5,5":["5,3","5,4","5,2","5,6"],
-    "5,6":["5,3","5,4","5,5","5,2"],
-    #################################
-    "6,2":["6,3","6,4","6,5","6,6"],
-    "6,3":["6,2","6,4","6,5","6,6"],
-    "6,4":["6,3","6,2","6,5","6,6"],
-    "6,5":["6,3","6,4","6,2","6,6"],
-    "6,6":["6,3","6,4","6,5","6,2"]
-    }
+def largeKakuroSolver():
     x=[1,2,3,4,5,6,7,8,9]
+    Variables=["2,2","2,3","2,5","2,6","3,2","3,3","3,4","3,5","3,6"
+    ,"4,3","4,4","4,5","5,2","5,3","5,4","5,5","5,6","6,2","6,3","6,5","6,6"]
     domain={v:x for v in Variables}
-    problem1=CSP(Variables,domain,neighbors,sixX6KakuroConstraints)
-    print(backtracking_search(problem1,select_unassigned_variable=mrv,order_domain_values=unordered_domain_values,inference=forward_checking))
+    neighbors={
+    "2,2":["2,3","2,5","2,6","3,2","5,2","6,2"],"2,3":["2,2","2,5","2,6","3,3","4,3","5,3","6,3"],
+    "2,5":["2,3","2,2","2,6","3,5","4,5","5,5","6,5"],"2,6":["2,3","2,5","2,2","3,6","5,6","6,6"],
+    "3,2":["3,3","3,4","3,5","3,6","5,2","6,2","2,2"], "3,3":["3,2","3,4","3,5","3,6","4,3","5,3","6,3"],"3,4":["3,3","3,2","3,5","3,6","4,4","5,4"],
+    "3,5":["3,3","3,4","3,2","3,6","4,5","5,5","6,5"], "3,6":["3,3","3,4","3,5","3,2","5,6","6,6"],
+    "4,3":["4,5","4,4","5,3","6,3","2,3"],
+    "4,4":["4,3","4,5","5,4","3,4"],
+    "4,5":["4,3","4,4","5,5","6,5","3,5","2,5"],
+    "5,2":["5,3","5,4","5,5","5,6","6,2","2,2","3,2"],
+    "5,3":["5,2","5,4","5,5","5,6","6,3"],"5,4":["5,3","5,2","5,5","5,6"],"5,5":["5,3","5,4","5,2","5,6","6,5"],
+    "5,6":["5,3","5,4","5,5","5,2","6,6"],
+    "6,2":["6,3","6,5","6,6"],
+    "6,3":["6,2","6,5","6,6"],
+    "6,5":["6,2","6,3","6,6"],
+    "6,6":["6,2","6,3","6,5"]
+    }
+    problem1=CSP(Variables,domain,neighbors,largeKakuroConstraints)
+    problem2=CSP(Variables,domain,neighbors,largeKakuroConstraints)
+    start=time.time()
+    result=backtracking_search(problem1,select_unassigned_variable=mrv,order_domain_values=unordered_domain_values,inference=forward_checking)
+    total=time.time()-start
+    print(result,"in time",total,"by FC+MRV+BT")
+    start=time.time()
+    AC3(problem2)
+    result2=backtracking_search(problem2,select_unassigned_variable=mrv)
+    total=time.time()-start
+    print(result2,"in time",total,"by AC3+BT+MRV")
 if __name__ == "__main__":
-    sixX6KakuroSolver()
+    largeKakuroSolver()
